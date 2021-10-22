4839257ad3dceeb8c3b61ec16dc2d31958979e71
Gribble2 <w.henderson1@lancaster.ac.uk>
2021-10-22 10:44:55 +0100

    Zoombar update
    Zoom bar now updates itself when the player zooms instead with the mouse wheel.

--- src/main/java/de/mat2095/my_slither/MySlitherCanvas.java
+++ src/main/java/de/mat2095/my_slither/MySlitherCanvas.java
@@ -30,7 +30,7 @@ final class MySlitherCanvas extends JPanel {
     private static final Color[] SNAKE_HALO_COLORS = new Color[]{new Color(0x60287BDE, true), new Color(0x00287BDE, true)};
     private static final Color[] OWN_SNAKE_HALO_COLORS = new Color[]{new Color(0x6039AFFF, true), new Color(0x0039AFFF, true)};
     private static final Color SNAKE_BODY_COLOR = new Color(0x6A8759);
-    private static Color OWN_SNAKE_BODY_COLOR = new Color(0xA5C261); // this needs to be updated with combo box option 
+    private static Color OWN_SNAKE_BODY_COLOR = new Color(0xA5C261); // this needs to be updated with combo box option
     private static final Color MAP_COLOR = new Color(0xA0A9B7C6, true);
     private static final Color MAP_POSITION_COLOR = new Color(0xE09E2927, true);
     private static final Color NAME_SHADOW_COLOR = new Color(0xC02B2B2B, true);
@@ -85,6 +85,7 @@ final class MySlitherCanvas extends JPanel {
             zoom -= e.getWheelRotation();
             zoom = Math.max(zoom, 0);
             zoom = Math.min(zoom, 18);
+            view.zoomBar.setValue(zoom);
         });
 
         addMouseMotionListener(new MouseMotionListener() {
